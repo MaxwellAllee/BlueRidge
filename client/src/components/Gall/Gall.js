9f4a5affba35c93ab587ce58d13d77a204bdb364
maxwellallee <maxwell.w.allee@gmail.com>
2019-08-20 19:49:51 -0400

    finished gallery V1

--- client/src/components/Gall/Gall.js
+++ client/src/components/Gall/Gall.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import API from '../../lib/API';
 import './Gall.css'
 import Modal from 'react-modal';
+import moment from 'moment'
 const customStyles = {
   content : {
     top                   : '50%',
@@ -17,13 +18,19 @@ class Gall extends Component {
     photos: {},
     mainPhoto: "",
     clickedPhoto:"",
-    modalIsOpen: false
+    modal:"",
+    modalIsOpen: false,
+    newest: true
   };
 
   componentDidMount() {
     API.Photos.allPhotos().then(res => {
       console.log(res.data)
-      this.setState({ photos: res.data, mainPhoto:res.data[0].photoName, clickedPhoto:res.data[0].photoName })
+      let photoz = res.data
+      this.setState({ photos: photoz, mainPhoto:photoz[0].photoName, clickedPhoto:photoz[0].photoName })
+      console.log(moment(photoz[0].createdAt).unix())
+      
+   
     })
   }
   hoverAction = photo => {
@@ -38,8 +45,9 @@ class Gall extends Component {
   selectThis =(photo)=>{
     this.setState({clickedPhoto:photo})
   }
-  openModal=()=> {
-    this.setState({modalIsOpen: true});
+  openModal=(photo)=> {
+    
+    this.setState({modalIsOpen: true, modal: photo});
   }
  
   afterOpenModal=()=> {
@@ -50,10 +58,22 @@ class Gall extends Component {
   closeModal=()=> {
     this.setState({modalIsOpen: false});
   }
+  sort = ()=>{
+    let holder = this.state.photos
+    console.log(holder)
+      holder.sort(function(a, b) {
+       console.log(moment(a.createdAt).unix())
+        a = moment(a.createdAt).unix();
+        b = moment(a.createdAt).unix();
+        return a<b ? -1 : a>b ? 1 : 0;
+    })
+      this.setState({photos:holder, newest:!this.state.newest})
+  }
   render() {
     let photos = this.state.photos
     return (
       <div className='Gall'>
+        
         <Modal
           isOpen={this.state.modalIsOpen}
           onAfterOpen={this.afterOpenModal}
@@ -61,18 +81,27 @@ class Gall extends Component {
           style={customStyles}
           contentLabel="Example Modal"
         >
- 
-          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-          <img src={`uploads/${this.state.mainPhoto}`} onClick={this.openModal} className="modalPhoto" alt="modal photo" />
+          
+          <img src={`uploads/${this.state.modal}`} 
+          ref={subtitle => this.subtitle = subtitle} 
+          className="modalPhoto" alt="modal photo" />
         </Modal>
-        <div class="gallGrid">
+        
+        <div id='switch'>
+          Sort By: {this.state.newest ? <span onClick={this.sort} className="switch"> Oldest</span> : 
+          <span onClick={this.sort} className="switch"> Newest </span>}
+        </div>
+        <div className="gallGrid">
+        
         {photos.length ? (photos.map(filez => {
           return (
 
 
             <div>
               <div>
-                <img src={`uploads/${filez.photoName}`} className="gallPhoto" onMouseOver={()=>{this.hoverAction(filez.photoName)}} onMouseOut={this.switchBack} onClick={()=>{this.selectThis(filez.photoName)}} alt={filez._id} />
+                <img src={`uploads/${filez.photoName}`} className="gallPhoto" 
+                onMouseOver={()=>{this.hoverAction(filez.photoName)}} onMouseOut={this.switchBack} 
+                onClick={()=>{this.openModal(filez.photoName)}} alt={filez._id} />
               </div>
 
             </div>)
@@ -80,10 +109,10 @@ class Gall extends Component {
             <h3>No Photos!</h3>
           )}
         
-          <div class="item1"></div>
-          <div class="item2"></div>
-          <div class="item3"></div>
-          <div class="focus"><img src={`uploads/${this.state.mainPhoto}`} onClick={this.openModal} className="mainPhoto" alt="main photo" /></div>
+          
+          <div className="focus"><img src={`uploads/${this.state.mainPhoto}`} 
+          onClick={()=>{this.openModal(this.state.mainPhoto)}} className="mainPhoto" 
+          alt="main photo" /></div>
           
 
         </div>
