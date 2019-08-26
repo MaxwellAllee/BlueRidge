35abf195a169ac16466640750158c3e582f7cae3
Max Allee <killercodingmonkey@gmail.com>
2019-08-26 09:49:26 -0400

    adding add page

--- client/src/components/PageForm/PageForm.js
+++ client/src/components/PageForm/PageForm.js
@@ -3,7 +3,7 @@ import Dropdown from 'react-dropdown'
 import AuthContext from '../../contexts/AuthContext';
 import API from '../../lib/API';
 import 'react-dropdown/style.css'
-
+import './pageForm.css'
 class PageForm extends Component {
     static contextType = AuthContext;
     state = {
@@ -11,76 +11,109 @@ class PageForm extends Component {
         availablePages: ["gallery", "test1", "test2"],
         allPages: [],
         selectedPage: -1,
-        editMode: true,
-        currentPage:{},
-        currentId:1,
-        currentStartingMiles:56,
-        currentFinishMiles:789,
-        currentSection:"BR",
-        currentBlog:"asdfasdf",
-        currentElevGain:23,
-        currentElevLoss:36
+        editMode: false,
+        currentPage: {},
+        currentId: "",
+        currentStartingMiles: "",
+        currentFinishMiles: "",
+        currentSection: "",
+        currentBlog: "",
+        currentElevGain: "",
+        currentElevLoss: ""
     };
-   
-    componentDidMount(){
-        API.Pages.allPages().then(res=>{
-          console.log(res.data)
-          this.setState({allPages:res.data})
-          let availPages = res.data.map(pages => pages.pageName)
-          availPages.unshift("New Page")
-          this.setState({availablePages: availPages});
-          
-      })}
-      handleInputChange(event) {
+
+    componentDidMount() {
+        API.Pages.allPages().then(res => {
+            console.log(res.data)
+            this.setState({ allPages: res.data })
+            let availPages = res.data.map(pages => pages.pageName)
+            availPages.unshift("New Page")
+            this.setState({ availablePages: availPages });
+
+        })
+    }
+    handleInputChange = (event) => {
         const target = event.target;
+        console.log(target)
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
-        console.log(value, name)
-        // this.setState({
-        //   [name]: value
-        // });
-      }
-      
-      _onSelect = option => {
-        let selected =this.state.availablePages.indexOf(option.value)     
-         this.setState({selectedPage:selected, editMode: true})
-         console.log(this.state.allPages[selected-1].id)
-         selected && this.setState({
-             currentPage: this.state.allPages[selected-1],
-             currentId: this.state.allPages[selected-1].id,
-             currentStartingMiles: this.state.allPages[selected-1].startingMiles,
-             currentFinishMiles: this.state.allPages[selected-1].finishMiles,
-             currentSection: this.state.allPages[selected-1].section,
-             currentBlog: this.state.allPages[selected-1].blog,
-             currentElevGain: this.state.allPages[selected-1].elevGain,
-             currentElevLoss: this.state.allPages[selected-1].elevLoss
-            })
-        
+
+        this.setState({
+            [name]: value
+        });
     }
-    
+    sectChange = option => {
+        this.setState({ currentSection: option.value })
+    }
+    _onSelect = option => {
+        let selected = this.state.availablePages.indexOf(option.value)
+        this.setState({ selectedPage: selected, editMode: true })
+        console.log(this.state.allPages[selected - 1].id)
+        selected && this.setState({
+            currentPage: this.state.allPages[selected - 1],
+            currentId: this.state.allPages[selected - 1].id,
+            currentStartingMiles: this.state.allPages[selected - 1].startingMiles,
+            currentFinishMiles: this.state.allPages[selected - 1].finishMiles,
+            currentSection: this.state.allPages[selected - 1].section,
+            currentBlog: this.state.allPages[selected - 1].blog,
+            currentElevGain: this.state.allPages[selected - 1].elevGain,
+            currentElevLoss: this.state.allPages[selected - 1].elevLoss
+        })
+
+    }
+
 
     render() {
         const defaultOption = this.state.location
         const pageNum = this.state.selectedPage
+        const sect = ['SD', 'BR', 'SM']
         console.log(pageNum)
         return (
             <div className='PageForm'>
                 <div className='jumbotron'>
-                        <form>
+                    <form>
+
                         {this.state.editMode ? (<div>
-                            <input className="form-control" type="number" 
-                            value ={this.state.currentStartingMiles} 
-                            onChange={this.handleInputChange} /> 
-                        </div>):(
-                        // <Dropdown id="dropDown" 
-                        // options={this.state.availablePages} 
-                        // onChange={this._onSelect} 
-                        // value={defaultOption} />
-                        <div></div>
-                        )}
-                        
-                        </form>
-                    
+                            <label>
+                                Starting Miles:
+                                <input name="currentStartingMiles" className="form-control" type="number"
+                                    value={this.state.currentStartingMiles}
+                                    onChange={this.handleInputChange} />
+                            </label>
+                            <label>
+                                Finishing Miles:
+                                <input name="currentFinishMiles" className="form-control" type="number"
+                                    value={this.state.currentFinishMiles}
+                                    onChange={this.handleInputChange} />
+                            </label>
+                            <br />
+                            <label>
+                                Section:
+                                    <Dropdown id="dropDown"
+                                    type='text'
+                                    options={sect}
+                                    onChange={this.sectChange}
+                                    value={defaultOption} />
+                            </label>
+                            <br />
+                            <label>
+                                Notes:
+                                <textarea name="currentBlog" style="{ width: 60vw; height:20vh;}"
+                            } className="form-control" type="text"
+                                    value={this.state.currentBlog}
+                                onChange={this.handleInputChange} />
+                            </label>
+
+                        </div>) : (
+                                <Dropdown id="dropDown"
+                                    options={this.state.availablePages}
+                                    onChange={this._onSelect}
+                                    value={defaultOption} />
+
+                            )}
+
+                    </form>
+
                 </div>
             </div>
         )
